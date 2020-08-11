03eb183c7aaac662c66c5e6e7ee9a74f603f1e80
wangjun <2665254503@qq.com>
2020-08-11 12:30:07 +0800

    用ListAPIView完成区域视图的改写

--- drf_meiduo/apps/area/views.py
+++ drf_meiduo/apps/area/views.py
@@ -8,9 +8,14 @@ from rest_framework.views import APIView
 from drf_meiduo.apps.area.serializers import AreaSearializer
 from . models import Area
 
-class AreasView(APIView):
+class AreasView(ListAPIView):
+    queryset = Area.objects.all()
+    serializer_class = AreaSearializer
 
-    def get(self,request):
-        queryset = Area.objects.filter(parent=None).all()
-        serializer = AreaSearializer(queryset,many=True)
-        return Response(serializer.data)
+    def get_queryset(self):
+        return Area.objects.filter(parent=None).all()
+
+    # def get(self,request):
+    #     queryset = Area.objects.filter(parent=None).all()
+    #     serializer = AreaSearializer(queryset,many=True)
+    #     return Response(serializer.data)
